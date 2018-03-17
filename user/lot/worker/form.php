da29fe960c647a960da89b07a620402be17f2d13
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-03-17 19:35:40 +0700

    Add `kick` Property

--- user/lot/worker/form.php
+++ user/lot/worker/form.php
@@ -1,12 +1,12 @@
-<form action="<?php echo $url . '/' . Extend::state('user', 'path') . $url->query('&amp;'); ?>" method="post" style="display:block;max-width:15em;margin-right:auto;margin-left:auto;">
+<form class="form-user form-user:<?php echo ($is_user = Is::user()) ? 'exit' : 'enter'; ?>" action="<?php echo $url . '/' . Extend::state('user', 'path') . $url->query('&amp;'); ?>" method="post" style="display:block;max-width:15em;margin-right:auto;margin-left:auto;">
   <?php echo $message; ?>
-  <?php if (!Is::user()): ?>
+  <?php if (!$is_user): ?>
   <p title="<?php echo $language->user; ?>"><?php echo Form::text('key', null, $language->user, ['class[]' => ['input', 'block']]); ?></p>
   <p title="<?php echo $language->pass; ?>"><?php echo Form::password('pass', null, $language->pass, ['class[]' => ['input', 'block']]); ?></p>
   <p><?php echo Form::submit('v', 1, $language->enter, ['class[]' => ['button']]); ?></p>
   <?php else: ?>
-  <p style="text-align:center;"><?php echo Form::submit('x', 1, $language->exit . ' (' . Is::user() . ')', ['class[]' => ['button']]) . ' ' . HTML::a($language->home, '/', false, ['class[]' => ['button']]); ?></p>
-  <?php echo Form::hidden('key', Is::user()); ?>
+  <p style="text-align:center;"><?php echo Form::submit('x', $is_user, $language->exit, ['class[]' => ['button'], 'title' => $is_user]) . ' ' . HTML::a($language->home, '/', false, ['class[]' => ['button']]); ?></p>
   <?php endif; ?>
   <?php echo Form::hidden('token', $token); ?>
+  <?php echo Form::hidden('kick', Request::get('kick', $url->previous)); ?>
 </form>
--- user/lot/worker/worker/route.php
+++ user/lot/worker/worker/route.php
@@ -16,10 +16,12 @@ Route::set($path, function() use($path, $language, $site) {
         // Log out!
         if ($is_enter) {
             // Check token…
-            if (empty($token) || !Guardian::check($token)) {
+            if (Is::void($token) || !Guardian::check($token)) {
                 Message::error('token');
+            } else if (!isset($r['x']) || Is::void($r['x'])) {
+                Message::error('void_field', $language->user, true);
             } else {
-                File::open(USER . DS . $key . DS . 'token.data')->delete();
+                File::open(USER . DS . $r['x'] . DS . 'token.data')->delete();
                 Session::reset('url.user');
                 Session::reset('url.pass');
                 Session::reset('url.token');
@@ -30,13 +32,13 @@ Route::set($path, function() use($path, $language, $site) {
         // Log in!
         } else {
             // Check token…
-            if (empty($token) || !Guardian::check($token)) {
+            if (Is::void($token) || !Guardian::check($token)) {
                 Message::error('token');
             // Check user name…
-            } else if (empty($key)) {
+            } else if (Is::void($key)) {
                 Message::error('void_field', $language->user, true);
             // Check user pass…
-            } else if (empty($pass)) {
+            } else if (Is::void($pass)) {
                 Message::error('void_field', $language->pass, true);
             // No error(s), go to the next step(s)…
             } else {
@@ -45,20 +47,16 @@ Route::set($path, function() use($path, $language, $site) {
                     // Reset password by deleting `pass.data` manually, then log in!
                     if (!file_exists($f = USER . DS . $key . DS . 'pass.data')) {
                         File::write(X . password_hash($pass . ' ' . $key, PASSWORD_DEFAULT))->saveTo($f, 0600);
-                        Message::success('create', [$language->pass, '<em>' . $pass . '</em>']);
+                        // Message::success('create', [$language->pass, '<em>' . $pass . '</em>']);
                     }
                     $enter = false;
                     $secret = File::open($f)->get(0, "");
                     // Validate password hash!
                     if (strpos($secret, X) === 0) {
-                        if (password_verify($pass . ' ' . $key, substr($secret, 1))) {
-                            $enter = true;
-                        }
+                        $enter = password_verify($pass . ' ' . $key, substr($secret, 1));
                     // Validate password!
                     } else {
-                        if ($pass === $secret) {
-                            $enter = true;
-                        }
+                        $enter = $pass === $secret;
                     }
                     // Is valid, then…
                     if ($enter) {
@@ -75,7 +73,7 @@ Route::set($path, function() use($path, $language, $site) {
                         // Show success message!
                         Message::success('user_enter');
                         // Redirect to the home page by default!
-                        Guardian::kick(isset($r['kick']) ? $r['kick'] : $url->previous);
+                        Guardian::kick(isset($r['kick']) ? $r['kick'] : "");
                     } else {
                         Message::error('user_or_pass');
                     }
