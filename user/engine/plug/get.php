1e2062b080a2630d47808ca8f425a72d6e8d5719
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-04-05 20:16:56 +0700

    @mecha-cms/genome/engine/kernel/genome.php

--- user/engine/plug/get.php
+++ user/engine/plug/get.php
@@ -1,9 +1,4 @@
 <?php
 
 // Based on `lot\extend\page\engine\plug\get.php`
-
-function fn_get_users($folder = USER, $state = 'page', $sort = [1, 'slug'], $key = null) {
-    return fn_get_pages($folder, $state, $sort, $key);
-}
-
-Get::_('users', 'fn_get_users');
+Get::_('users', ['fn_get_pages', [USER, 'page', [1, 'slug'], null]]);
