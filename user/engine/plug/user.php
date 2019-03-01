0cfdaa96fa2e5c434dac4d5b4fdc39ab515e7b5b
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-03-01 23:25:11 +0700

    Update

--- user/engine/plug/user.php
+++ user/engine/plug/user.php
@@ -3,3 +3,5 @@
 // Based on `.\lot\extend\page\engine\plug\page.php`
 User::_('time', "fn\\page\\time");
 User::_('update', "fn\\page\\update");
+
+User::$data = extend([], (array) Config::get('page', true), (array) Config::get('user', true));
