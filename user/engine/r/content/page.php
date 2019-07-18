6cdf60e318d6f93379b3821bc3c145eb471c6a5b
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-07-18 11:55:54 +0700

    Tidy

--- user/engine/r/content/page.php
+++ user/engine/r/content/page.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
-<html class dir="<?php echo $site->direction; ?>" style="margin:0;padding:0;width:100%;height:100%;display:block;overflow:hidden;">
+<html class dir="<?php echo $site->direction; ?>" style="
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  display: block;
+  overflow: hidden;
+">
   <head>
     <meta charset="<?php echo $site->charset; ?>">
     <meta content="width=device-width" name="viewport">
@@ -7,8 +14,21 @@
     <title><?php echo w($t->reverse); ?></title>
     <link href="<?php echo $url; ?>/favicon.ico" rel="shortcut icon">
   </head>
-  <body style="margin:0;padding:0;width:100%;height:100%;display:flex;align-contents:center;align-items:center;">
-    <div style="flex:1;margin:0 auto;padding:0;max-width:15em;">
+  <body style="
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-contents: center;
+    align-items: center;
+  ">
+    <div style="
+      flex: 1;
+      margin: 0 auto;
+      padding: 0;
+      max-width: 15em;
+    ">
       <?php static::form('user'); ?>
     </div>
   </body>
