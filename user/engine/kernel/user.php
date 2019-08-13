b602e59f0c0eaf2f072798abc3f3823aa22b7736
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-08-13 10:00:28 +0700

    Update

--- user/engine/kernel/user.php
+++ user/engine/kernel/user.php
@@ -7,19 +7,19 @@ class User extends Page {
     }
 
     public function __toString() {
-        if (is_string($v = $this->offsetGet('author'))) {
+        if (is_string($v = parent::offsetGet('author'))) {
             return $v;
         }
-        return (string) $this->user;
+        return (string) $this->user();
     }
 
-    public function URL() {
-        $n = $this->exist ? $this->name : null;
+    public function URL(...$lot) {
+        $n = $this->exist ? parent::name() : null;
         return $n ? $GLOBALS['url'] . '/' . state('user')['/'] . '/' . $n : null;
     }
 
     public function user() {
-        return $this->exist ? '@' . $this->name : null;
+        return $this->exist ? '@' . parent::name() : null;
     }
 
 }
