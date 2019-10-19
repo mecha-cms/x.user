e5957ad292a5c01eddbb92be60a6241352c05447
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-10-19 22:08:42 +0700

    Test

--- user/index.php
+++ user/index.php
@@ -8,8 +8,9 @@ require __DIR__ . DS . 'engine' . DS . 'r' . DS . 'state.php';
 require __DIR__ . DS . 'engine' . DS . 'r' . DS . 'user.php';
 
 // Apply route(s) only if we have at least one user
-if (q(g(USER, 'page')) === 0) {
-    require __DIR__ . DS . 'engine' . DS . 'r' . DS . 'route' . DS . 'set.php';
-} else {
+if (q(g(USER, 'page')) > 0) {
     require __DIR__ . DS . 'engine' . DS . 'r' . DS . 'route.php';
+// Else, prompt author to create an user account
+} else {
+    require __DIR__ . DS . 'engine' . DS . 'r' . DS . 'route' . DS . 'set.php';
 }
