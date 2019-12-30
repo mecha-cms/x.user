bda07bc8e5e992a9dffeceb79c293b223397152d
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-12-31 00:28:02 +0700

    Form

--- user/engine/r/layout/exit.php
+++ user/engine/r/layout/exit.php
@@ -3,7 +3,9 @@
 $_state = State::get('x.user', true);
 
 ?>
-<?= $alert; ?>
-<p>
-  <a class="button" href="<?= $url . $_state['path'] . '/' . $user->name; ?>"><?= i('Profile'); ?></a> <a class="button" href="<?= $url . $_state['path'] . '/' . $user->name . $url->query('&amp;', ['exit' => $user->token]); ?>" title="<?= $user->user; ?>"><?= i('Exit'); ?></a>
-</p>
+<form class="form-user form-user:exit">
+  <?= $alert; ?>
+  <p>
+    <a class="button" href="<?= $url . $_state['path'] . '/' . $user->name; ?>"><?= i('Profile'); ?></a> <a class="button" href="<?= $url . $_state['path'] . '/' . $user->name . $url->query('&amp;', ['exit' => $user->token]); ?>" title="<?= $user->user; ?>"><?= i('Exit'); ?></a>
+  </p>
+</form>
