a6bd6d315a927c0124a89b3e92f222420ed52f4a
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-12-31 08:33:09 +0700

    Align center.

--- user/engine/r/layout/exit.php
+++ user/engine/r/layout/exit.php
@@ -5,7 +5,9 @@ $_state = State::get('x.user', true);
 ?>
 <form class="form-user form-user:exit">
   <?= $alert; ?>
-  <p>
+  <p style="
+    text-align: center;
+  ">
     <a class="button" href="<?= $url . $_state['path'] . '/' . $user->name; ?>"><?= i('Profile'); ?></a> <a class="button" href="<?= $url . $_state['path'] . '/' . $user->name . $url->query('&amp;', ['exit' => $user->token]); ?>" title="<?= $user->user; ?>"><?= i('Exit'); ?></a>
   </p>
 </form>
