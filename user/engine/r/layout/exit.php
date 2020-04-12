4a62643080270350255fdff6507c37cc927a19f5
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-04-12 09:44:11 +0700

    target=_top

--- user/engine/r/layout/exit.php
+++ user/engine/r/layout/exit.php
@@ -8,6 +8,6 @@ $_state = State::get('x.user', true);
   <p style="
     text-align: center;
   ">
-    <a class="button" href="<?= $url . $_state['path'] . '/' . $user->name; ?>"><?= i('Profile'); ?></a> <a class="button" href="<?= $url . $_state['path'] . '/' . $user->name . $url->query('&amp;', ['exit' => $user->token]); ?>" title="<?= $user->user; ?>"><?= i('Exit'); ?></a>
+    <a class="button" href="<?= $url . $_state['path'] . '/' . $user->name; ?>"><?= i('Profile'); ?></a> <a class="button" href="<?= $url . $_state['path'] . '/' . $user->name . $url->query('&amp;', ['exit' => $user->token]); ?>" target="_top" title="<?= $user->user; ?>"><?= i('Exit'); ?></a>
   </p>
 </form>
