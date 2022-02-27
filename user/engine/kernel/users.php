f89657f260135a60c61ba6a7f5490332f1bb4135
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-02-27 18:45:26 +0700

    Accept Array Input

--- user/engine/kernel/users.php
+++ user/engine/kernel/users.php
@@ -2,11 +2,14 @@
 
 class Users extends Pages {
 
-    public function page(string $path) {
-        return new User($path);
+    public function page(string $path = null, array $lot = []) {
+        return new User($path, $lot);
     }
 
     public static function from(...$lot) {
+        if (is_array($v = reset($lot))) {
+            return parent::from($v);
+        }
         $lot[0] = $lot[0] ?? LOT . D . 'user';
         return parent::from(...$lot);
     }
