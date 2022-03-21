597feece78a628a55c438969f9eae43af069d325
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-03-21 13:23:16 +0700

    Update

--- user/engine/kernel/user.php
+++ user/engine/kernel/user.php
@@ -2,12 +2,6 @@
 
 class User extends Page {
 
-    public function __construct(string $path = null, array $lot = []) {
-        $c = c2f(self::class);
-        parent::__construct($path, array_replace_recursive((array) State::get('x.' . $c . '.page', true), $lot));
-        $this->hook[] = $c;
-    }
-
     public function __toString() {
         if (is_string($v = parent::author())) {
             return $v;
