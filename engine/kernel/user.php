4579bd8777a07aa0e7b36d6e6de7ad0e0addd670
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-04-25 06:44:30 +0700

    Move to Root

--- engine/kernel/user.php
+++ engine/kernel/user.php
@@ -16,11 +16,11 @@ class User extends Page {
     public function URL(...$lot) {
         extract($GLOBALS, EXTR_SKIP);
         $n = $this->exist() ? parent::name() : null;
-        return $n ? $url . ($state->x->user->path ?? '/user') . '/' . $n : null;
+        return $n ? $url . ($state->x->user->route ?? '/user') . '/' . $n : null;
     }
 
     public function user() {
-        return $this->exist() ? '@' . parent::name() : null;
+        return $this->exist ? '@' . parent::name() : null;
     }
 
 }
