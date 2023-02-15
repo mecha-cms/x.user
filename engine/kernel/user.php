ba8bcbcb83351b7aa06fc82ff8be9fb4fdcb79fc
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-02-15 23:07:48 +0700

    Consistent Icon Color

--- engine/kernel/user.php
+++ engine/kernel/user.php
@@ -16,7 +16,7 @@ class User extends Page {
     public function URL(...$lot) {
         extract($GLOBALS, EXTR_SKIP);
         $n = $this->_exist() ? parent::name() : null;
-        return $n ? $url . ($state->x->user->route ?? '/user') . '/' . $n : null;
+        return $n ? $url . '/' . trim($state->x->user->route ?? 'user', '/') . '/' . $n : null;
     }
 
     public function user() {
