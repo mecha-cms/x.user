640d964709036f7e989ce8ccca25273a80b2b31b
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-05-11 22:07:23 +0700

    Update

--- engine/kernel/users.php
+++ engine/kernel/users.php
@@ -7,9 +7,6 @@ class Users extends Pages {
     }
 
     public static function from(...$lot) {
-        if (is_array($v = reset($lot))) {
-            return parent::from($v);
-        }
         $lot[0] = $lot[0] ?? LOT . D . 'user';
         return parent::from(...$lot);
     }
