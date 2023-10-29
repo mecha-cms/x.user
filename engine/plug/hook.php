b2d514ceb5f91a9db9ca32e1f9f9eaff42d716b4
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-10-30 00:49:26 +0700

    Update

--- engine/plug/hook.php
+++ engine/plug/hook.php
@@ -1,3 +1,17 @@
 <?php
 
-$GLOBALS['user'] = $user = new User(($n = cookie('user.name')) ? LOT . D . 'user' . D . $n . '.page' : null);
+// By default, extension(s) are loaded in alphabetical order. The most obvious example is the loading order of Panel and
+// User extension. Panel extension will be loaded before User extension even though Panel depends on User. It is
+// necessary to make sure that User extension is loaded before Panel extension. However, because of the extension’s
+// default loading order, it is currently not possible to load User extension before Panel extension until I can find
+// another way to allow individual extension to be loaded in custom order. Plug file(s) can be used as a dirty solution
+// for now to load other extension requirement(s) early, just before the target extension’s `index.php` file is loaded.
+//
+// There is no particular reason why I chose this `hook.php` file, except that it (the `Hook` class) is the most ideal
+// class to watch its initial loading event because most extension(s) will call it in their `index.php` file, so it will
+// immediately trigger the `$user` variable below to be declared. There are several other class(es) that are loaded much
+// earlier than the `Hook` class, such as the `State` and the `URL` class(es), but they are loaded before the class
+// and plug file(s) from other extension(s) are loaded, so there will be more potential for unpredictable error(s) due
+// to declaring the `User` class too early.
+
+$GLOBALS['user'] = $user = new User(is_file($f = LOT . D . 'user' . D . (cookie('user.name') ?? P) . '.page') ? $f : null);
