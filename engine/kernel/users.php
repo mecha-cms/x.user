c9b11f2a4176b3e06a153883510ea13d6bfaa7ac
Taufik Nurrohman <hi@taufik-nurrohman.com>
2026-03-12 20:09:05 +0700

    Update

--- engine/kernel/users.php
+++ engine/kernel/users.php
@@ -3,14 +3,11 @@
 class Users extends Pages {
 
     public function page(...$lot) {
-        if (($v = $lot[0] ?? 0) instanceof User) {
-            return $v;
+        static $c = [];
+        if (isset($c[$id = json_encode($lot)])) {
+            return $c[$id];
         }
-        if (is_array($v)) {
-            unset($v[P]);
-            $lot[0] = $v;
-        }
-        return new User(...$lot);
+        return ($c[$id] = new User(...$lot));
     }
 
     public static function from(...$lot) {
